6568032b6c029cb7165bd62a6af177731e0de8b4
hussainibulama <43305254+hussainibulama@users.noreply.github.com>
2021-07-10 04:56:13 +0100

    A

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/pipeline">
+    <BrowserRouter basename={window.location.pathname || ""}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
