a9fed1261f98ddde4762f3ce288e463ed7c7173d
hussainibulama <43305254+hussainibulama@users.noreply.github.com>
2021-07-10 05:19:26 +0100

    a

--- src/components/main.js
+++ src/components/main.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { Switch, Route, Router } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import Home from "./home";
 
 const Main = () => (
-  <Router basename="/pipeline">
-    <Switch>
-      <Route exact path="/" component={Home} />
-    </Switch>
-  </Router>
+  <Switch>
+    <Route exact path="/" component={Home} />
+  </Switch>
 );
 
 export default Main;
